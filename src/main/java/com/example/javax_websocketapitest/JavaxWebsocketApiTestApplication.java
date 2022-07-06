636093552c041f25dd0a26bfd04dc4cf7b379163
Madushan Sandaruwan <madushansandaru1@gmail.com>
2022-07-06 09:26:57 +0530

    Bootstrap the project

---
+++ src/main/java/com/example/javax_websocketapitest/JavaxWebsocketApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.javax_websocketapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaxWebsocketApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaxWebsocketApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javax_websocketapitest/JavaxWebsocketApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javax_websocketapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaxWebsocketApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
